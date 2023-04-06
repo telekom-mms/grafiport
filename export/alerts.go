80bbda5f9951336fba23ac09f1e428010b8c24ed
phpe <p.penquitt@t-systems.com>
2023-04-06 14:41:19 +0200

    feat: go mod tidy mal laufen lassen

--- export/alerts.go
+++ export/alerts.go
@@ -1,14 +1,14 @@
 package export
 
 import (
-	"encoding/json"
+	_ "encoding/json"
 	"fmt"
-	"log"
+	_ "log"
 	url2 "net/url"
 	"os"
 	"path/filepath"
 
-	"github.com/gosimple/slug"
+	_ "github.com/gosimple/slug"
 	gapi "github.com/grafana/grafana-api-golang-client"
 )
 
@@ -30,27 +30,31 @@ func Alerts(username, password, url, directory string) {
 		os.Mkdir(path, 0760)
 	}
 
-// wir benötigen erst alle Folder mit den ID's bevor wir uns die AlertRules holen können, da diese an den folder hängen
-	folders, err := client.Folders()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to create search dashboards: %s\n", err)
-		os.Exit(1)
-	}
-	for _, folder := range folders {
-		f, _ := client.FolderByUID(folder.UID)
-		title := f.Title
-		fuid := f.UID
-		fmt.Println(fuid, title)
-		arg, err := client.AlertRuleGroup(fuid, title)
-		if err != nil {
-			log.Printf("Konnte AlertRuleGroup nicht beziehen %v %v", fuid, title)
-			continue
-		}
-		ar, err := client.AlertRule(arg.FolderUID)
-		if err != nil {
-			log.Fatalf("Konnte die AlertRule nicht beziehen %v", err)
-		}
-		jsonFolder, _ := json.Marshal(ar)
-		_ = os.WriteFile(filepath.Join(path, slug.Make(ar.Title))+".json", jsonFolder, os.FileMode(int(0666)))
-	}
+	v := url2.Values{}
+	v.Set("state", "firing")
+	alerts, _ := client.Alerts(v)
+	fmt.Println(alerts, v)
+// // wir benötigen erst alle Folder mit den ID's bevor wir uns die AlertRules holen können, da diese an den folder hängen
+// 	folders, err := client.Folders()
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "Failed to create search dashboards: %s\n", err)
+// 		os.Exit(1)
+// 	}
+// 	for _, folder := range folders {
+// 		f, _ := client.FolderByUID(folder.UID)
+// 		title := f.Title
+// 		fuid := f.UID
+// 		fmt.Println(fuid, title)
+// 		arg, err := client.AlertRuleGroup(fuid, title)
+// 		if err != nil {
+// 			log.Printf("Konnte AlertRuleGroup nicht beziehen %v %v", fuid, title)
+// 			continue
+// 		}
+// 		ar, err := client.AlertRule(arg.FolderUID)
+// 		if err != nil {
+// 			log.Fatalf("Konnte die AlertRule nicht beziehen %v", err)
+// 		}
+// 		jsonFolder, _ := json.Marshal(ar)
+// 		_ = os.WriteFile(filepath.Join(path, slug.Make(ar.Title))+".json", jsonFolder, os.FileMode(int(0666)))
+// 	}
 }
--- go.mod
+++ go.mod
@@ -5,14 +5,12 @@ go 1.19
 require (
 	github.com/gosimple/slug v1.1.1
 	github.com/grafana-tools/sdk v0.0.0-20220919052116-6562121319fc
-	github.com/spf13/cobra v1.6.1
+	github.com/grafana/grafana-api-golang-client v0.18.4
 )
 
 require (
-	github.com/grafana/grafana-api-golang-client v0.18.4 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rainycape/unidecode v0.0.0-20150907023854-cb7f23ec59be // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
